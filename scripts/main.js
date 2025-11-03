7204eac3bdbaede5ae4db695db22f38ac6f793cd
Oleg Ponomarjov <erep4@inbox.lv>
2025-11-03 18:45:39 +0200

    add some code with problem, need to find issue.

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,13 @@ import ExpandableContentCollection from "./ExpandableContent.js";
 import InputMaskCollection from "./InputMask.js";
 import SelectCollection from "./Select.js";
 
+
+
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
 new ExpandableContentCollection();
 new InputMaskCollection();
 new SelectCollection();
+
+
